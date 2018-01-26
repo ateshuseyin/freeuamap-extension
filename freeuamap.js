3650375770b518a323d108fd8c7e597f2313e992
Hüseyin Ateş <huseyin.ates@ozguryazilim.com.tr>
2018-01-26 11:27:25 +0300

    added several map options

--- freeuamap.js
+++ freeuamap.js
@@ -1,12 +1,52 @@
+/*global $,map*/
 $(document).ready(function () {
-   var button = document.querySelector('.gotoothermap');
+    var button = document.querySelector('.gotoothermap');
+    var originalMapLayer;
+    map.eachLayer(function (layer) {
+        if (!originalMapLayer &&
+            layer._url &&
+            layer._url.indexOf('maps.liveuamap.com/osm_tiles/') !== -1) {
+            originalMapLayer = layer;
+        }
+    });
 
-   var current = 0;
+    var esriWorld = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
 
-   //https://maps.liveuamap.com/osm_tiles/{z}/{x}/{y}.png
+    var esriNatGeoWorldMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+        maxZoom: 16
+    });
 
-   button.onclick = function (event) {
+    var openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        maxZoom: 17,
+        attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+
+    var NASAGIBS_ViirsEarthAtNight2012 = L.tileLayer('https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
+        attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
+        bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
+        minZoom: 1,
+        maxZoom: 8,
+        format: 'jpg',
+        time: '',
+        tilematrixset: 'GoogleMapsCompatible_Level'
+    });
+
+    var layers = [originalMapLayer,
+        esriWorld,
+        esriNatGeoWorldMap,
+        openTopoMap,
+        NASAGIBS_ViirsEarthAtNight2012];
+
+    var activeIndex = 0;
+
+    button.onclick = function (event) {
         event.preventDefault();
+        map.removeLayer(layers[activeIndex]);
+        activeIndex = (activeIndex + 1) % layers.length;
+        map.addLayer(layers[activeIndex]);
         return false;
-   };
+    };
 });
